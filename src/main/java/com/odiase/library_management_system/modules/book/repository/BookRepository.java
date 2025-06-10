1a2fd0f2a8012f9e35f84807e23123b890e71039
odiasesamuel <odiasesamuel4@gmail.com>
2025-06-10 14:16:10 +0100

    optimize queries fetching list of books and borrow records using @EntityGraph

--- src/main/java/com/odiase/library_management_system/modules/book/repository/BookRepository.java
+++ src/main/java/com/odiase/library_management_system/modules/book/repository/BookRepository.java
@@ -1,20 +1,28 @@
 package com.odiase.library_management_system.modules.book.repository;
 
 import com.odiase.library_management_system.modules.book.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @EntityGraph(attributePaths = {"genre"})
+    Page<Book> findAll(Pageable pageable);
+
     Optional<Book> findByTitleIgnoreCase(String title);
 
+    @EntityGraph(attributePaths = {"genre"})
     List<Book> findByAuthor(String author);
 
+    @EntityGraph(attributePaths = {"genre"})
     List<Book> findByGenreId(Long genreId);
 
+    @EntityGraph(attributePaths = {"genre"})
     @Query("SELECT b FROM Book b WHERE b.availableCopies > 0")
     List<Book> findAllAvailableBooks();
 }
--- src/main/java/com/odiase/library_management_system/modules/borrowRecord/repository/BorrowRecordRepository.java
+++ src/main/java/com/odiase/library_management_system/modules/borrowRecord/repository/BorrowRecordRepository.java
@@ -1,12 +1,20 @@
 package com.odiase.library_management_system.modules.borrowRecord.repository;
 
 import com.odiase.library_management_system.modules.borrowRecord.entity.BorrowRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Long> {
+    @EntityGraph(attributePaths = {"user", "book"})
     List<BorrowRecord> findByBookId(Long bookId);
 
+    @EntityGraph(attributePaths = {"user", "book"})
+    Page<BorrowRecord> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"user", "book"})
     List<BorrowRecord> findByUserId(Long userId);
 }
